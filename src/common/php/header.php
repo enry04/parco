634ef8f51241b1ca258d059a113330fa07803470
enrico visentin <enrico04v@gmail.com>
2023-01-07 14:04:12 +0100

    init hamburger btn

--- src/common/php/header.php
+++ src/common/php/header.php
@@ -1,24 +1,26 @@
 <header>
     <div class="header-container">
         <div class="left-header-container">
-            <h3>
+            <h4>
                 <a <?= ($page == "main") ? $active : '' ?> class="header-option" href="../main-page/main-page.php">Home</a>
-            </h3>
-            <h3>
+            </h4>
+            <h4>
                 <a <?= ($page == "contact-us") ? $active : '' ?> class="header-option" href="../contact-us-page/contact-us-page.php">Contattaci</a>
-            </h3>
+            </h4>
         </div>
         <h4 class="current-park-text"></h4>
         <div class="logo-container"></div>
         <div class="right-header-container">
-            <h3>
+            <h4>
                 <a <?= ($page == "parks") ? $active : '' ?> class="header-option" href="../parks-page/parks-page.php"> I nostri parchi</a>
-            </h3>
+            </h4>
             <div class="hamburger-container">
+                <input type="checkbox" class="option-btn">
                 <div class="bar1"></div>
                 <div class="bar2"></div>
                 <div class="bar3"></div>
             </div>
+            </input>
         </div>
     </div>
 </header>
